8eff52204c3a127ed5b11e391fe11bc2d7555a9e
hayatoseki <hayato.dev.8810@gmail.com>
2023-12-05 14:26:45 +0900

    test04

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const server = app.listen(PORT, HOST, () => {
   console.log(`Running on http://${HOST}:${PORT}`);
 });
 
-//root
+//read
 app.get("/", (req, res) => {
   const read_query = {
     text: "SELECT * FROM entries;",
@@ -36,6 +36,25 @@ app.get("/", (req, res) => {
     .then((result) => {
       console.log(result.rows);
       res.json(result.rows);
+    })
+    .catch((e) => {
+      console.error(e.stack);
+      res.send("NG");
+      // process.exit(1);
+    });
+});
+
+//write
+app.post("/write", (req, res) => {
+  const write_query = {
+    text: "INSERT INTO entries (guestName, content) values ('first guest', 'I got here!');",
+  };
+
+  client
+    .query(write_query)
+    .then((result) => {
+      console.log(result);
+      res.json(result);
     })
     .catch((e) => {
       console.error(e.stack);
